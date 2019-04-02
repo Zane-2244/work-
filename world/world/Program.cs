ba4796896920c032ce0f9e72bc54aa205ed30dbc
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-02 08:44:14 -0700

    Add files via upload

---
+++ world/world/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 77;
+            int count = 20;
+            float totalprice = 30.44f;
+            char character = 'A';
+            string firstname = "Zane";
+            bool isworking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalprice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstname);
+            Console.WriteLine(isworking);  
+
+        }
+    }
+}
